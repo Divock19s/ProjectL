c982de72fe103c41b4b2d61a9f672ef98a39355d
Divock19s <116955660+Divock19s@users.noreply.github.com>
2022-11-18 00:56:45 +0100

    add git

--- Logos.gd
+++ Logos.gd
@@ -4,6 +4,6 @@ func _ready():
 	$AnimationPlayer.play("Start")
 	$Timer.start()
 func _on_AnimationPlayer_animation_finished(_anim_name):
-	var _k = get_tree().change_scene("res://World.tscn")
+	var _k = get_tree().change_scene("res://Menu/MainManu.tscn")
 func _on_Timer_timeout():
-	var _k = get_tree().change_scene("res://World.tscn")
+	var _k = get_tree().change_scene("res://Menu/MainManu.tscn")
